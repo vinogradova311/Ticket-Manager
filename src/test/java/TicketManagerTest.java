a06b11aafd98c599e3a1817932a4424bac762180
vinogradova311 <anny.amelie@gmail.com>
2022-10-19 19:54:46 +0300

    Comparator

--- src/test/java/TicketManagerTest.java
+++ src/test/java/TicketManagerTest.java
@@ -1,17 +1,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class TicketManagerTest {
 
     TicketRepository repo = new TicketRepository();
     TicketManager manager = new TicketManager(repo);
+    TicketsByTimeComparator timeComparator = new TicketsByTimeComparator();
 
-    Ticket item1 = new Ticket(246, 1999, "LED", "SVO",90);
-    Ticket item2 = new Ticket(249, 1999, "SVO", "LED",90);
-    Ticket item3 = new Ticket(12, 5999, "SVO", "LED",80);
-    Ticket item4 = new Ticket(1937, 1580, "LED", "VKO",110);
-    Ticket item5 = new Ticket(2043, 1580, "LED", "VKO",120);
-    Ticket item6 = new Ticket(2, 1799, "LED", "SVO",95);
+    Ticket item1 = new Ticket(246, 1999, "LED", "SVO", 90);
+    Ticket item2 = new Ticket(249, 1999, "SVO", "LED", 90);
+    Ticket item3 = new Ticket(12, 5999, "SVO", "LED", 80);
+    Ticket item4 = new Ticket(1937, 1580, "LED", "VKO", 110);
+    Ticket item5 = new Ticket(2043, 1580, "LED", "VKO", 120);
+    Ticket item6 = new Ticket(2, 1799, "LED", "SVO", 95);
 
     @Test
     public void addTest() {
@@ -22,23 +24,25 @@ public class TicketManagerTest {
         manager.add(item5);
         manager.add(item6);
 
-        Ticket[] expected = {item1, item2, item3, item4,item5, item6,};
+        Ticket[] expected = {item1, item2, item3, item4, item5, item6,};
         Ticket[] actual = repo.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
-@Test
-    public void searchTest() {
-    manager.add(item1);
-    manager.add(item2);
-    manager.add(item3);
-    manager.add(item4);
-    manager.add(item5);
-    manager.add(item6);
 
-    Ticket[] expected = {item6, item1};
-    Ticket[] actual = manager.searchBy("LED", "SVO");
-    Assertions.assertArrayEquals(expected, actual);
-}
+    @Test
+    public void searchTest() {
+        manager.add(item1);
+        manager.add(item2);
+        manager.add(item3);
+        manager.add(item4);
+        manager.add(item5);
+        manager.add(item6);
+
+        Ticket[] expected = {item6, item1};
+        Ticket[] actual = manager.searchBy("LED", "SVO");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
     @Test
     public void search2Test() {
         manager.add(item1);
@@ -76,10 +80,11 @@ public class TicketManagerTest {
         manager.add(item5);
         manager.add(item6);
 
-        Ticket[] expected = { };
+        Ticket[] expected = {};
         Ticket[] actual = manager.searchBy("VKO", "SVO");
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     public void removeByIdTest() {
         manager.add(item1);
@@ -96,4 +101,36 @@ public class TicketManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+
+    public void searchAndSortByTimeTest() {
+
+
+        manager.add(item1);
+        manager.add(item2);
+        manager.add(item3);
+        manager.add(item4);
+        manager.add(item5);
+        manager.add(item6);
+
+        Ticket[] expected = {item1, item6};
+        Ticket[] actual = manager.searchByAndSortByTime("LED", "SVO", timeComparator);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchAndSortByTime2Test() {
+        manager.add(item1);
+        manager.add(item2);
+        manager.add(item3);
+        manager.add(item4);
+        manager.add(item5);
+        manager.add(item6);
+
+        Ticket[] expected = {item3, item2};
+        Ticket[] actual = manager.searchByAndSortByTime("SVO", "LED", timeComparator);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
